2252f85801838e78566c5991aa6465cb91d81eed
zxdongyu <130543619+zxdongyu@users.noreply.github.com>
2024-08-06 13:10:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.dongyu.eu.org": dockerHub,
+  "quay.dongyu.eu.org": "https://quay.io",
+  "gcr.dongyu.eu.org": "https://gcr.io",
+  "k8s-gcr.dongyu.eu.org": "https://k8s.gcr.io",
+  "k8s.dongyu.eu.org": "https://registry.k8s.io",
+  "ghcr.dongyu.eu.org": "https://ghcr.io",
+  "cloudsmith.dongyu.eu.org": "https://docker.cloudsmith.io",
+  "ecr.dongyu.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.dongyu.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
